146cb624aaadbe00310a1395965cc674b55fe2e2
demitelix <demiteli@inbox.lv>
2020-02-29 23:15:38 +0200

    complete Lists

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -122,7 +122,11 @@ fun abs(v: List<Double>): Double = TODO()
  *
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
-fun mean(list: List<Double>): Double = TODO()
+fun mean(list: List<Double>): Double {
+    return if (list.sumByDouble { it } > 0) {
+        list.average()
+    } else 0.0
+}
 
 /**
  * Средняя
@@ -132,7 +136,15 @@ fun mean(list: List<Double>): Double = TODO()
  *
  * Обратите внимание, что данная функция должна изменять содержание списка list, а не его копии.
  */
-fun center(list: MutableList<Double>): MutableList<Double> = TODO()
+fun center(list: MutableList<Double>): List<Double> {
+    if (list.isNotEmpty()) {
+        val average = list.average()
+        // for (i in 0..list.size-1){list[i] -= average}
+        list.replaceAll { e -> e - average }
+        return list
+    }
+    return list
+}
 
 /**
  * Средняя
